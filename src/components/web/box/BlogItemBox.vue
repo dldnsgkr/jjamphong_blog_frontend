260e5d6300c910df3a47312c670a0967df3e519b
Lee UnHak <uhlee@cyglobal.net>
2024-09-17 13:26:47 +0900

    refactor: Set blog list item dummy data and screen (css) on main screen

--- src/components/web/box/BlogItemBox.vue
+++ src/components/web/box/BlogItemBox.vue
@@ -1,21 +1,108 @@
-<script setup></script>
+<script setup lang="ts">
+import mokup_image from '@assets/img/mokup_blog_item.png';
+import icon_black_heart from '@assets/icon/icon-black-heart.svg';
+
+type mokup_blog_item_listPropsType = {
+  title: string;
+  sub_title: string;
+  current_date: string;
+  comment_count: number;
+  nickName: string;
+  writer: string;
+  like_count: number;
+};
+
+const props = defineProps<{
+  mokup_blog_item_list: mokup_blog_item_listPropsType;
+}>();
+
+// typeScript 기본값 적용을 원할시 부모에서 객체를 분할 시켜서 보내줘야함
+// const props = withDefaults(
+//   defineProps<mokup_blog_item_listPropsType>(),
+//   {
+//     title: '',
+//     sub_title: '',
+//     current_date: '9999-12-31',
+//     comment_count: 0,
+//     nickName: 'unknown',
+//     writer: 'unknown',
+//     like_count: 0,
+//   }
+// );
+
+// js
+// const runtimeProps = defineProps({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   sub_title: {
+//     type: String,
+//     required: true,
+//   },
+//   current_date: {
+//     type: String,
+//     required: true,
+//   },
+//   comment_count: {
+//     type: Number,
+//     default: 0,
+//   },
+//   nickName: {
+//     type: String,
+//     default: 'unknown',
+//   },
+//   writer: {
+//     type: String,
+//     default: 'unknown',
+//   },
+//   like_count: {
+//     type: Number,
+//     default: 0,
+//   },
+// });
+</script>
 <template>
   <div class="blog-item-wrapper">
-    <img />
+    <img
+      v-if="mokup_image"
+      :src="mokup_image"
+      alt="Blog Image"
+    />
     <div class="blog-item-text">
-      <div>
-        <p>title title title title</p>
-        <span>subtitle subtitle</span>
+      <div class="title-wrapper">
+        <p class="main-title">
+          {{ props.mokup_blog_item_list.title }}
+        </p>
+        <p class="sub-title">
+          {{ props.mokup_blog_item_list.sub_title }}
+        </p>
       </div>
-      <div></div>
+      <div class="etc-wrapper">
+        <p class="date-comment">
+          {{ props.mokup_blog_item_list.current_date }} ·
+          {{
+            props.mokup_blog_item_list.comment_count.toLocaleString()
+          }}개의 댓글
+        </p>
+      </div>
     </div>
     <div class="blog-item-information">
       <div>
         <div class="writer-image"></div>
-        <div class="writer-name"></div>
+        <div class="writer-name">
+          <span>by </span>
+          <p>
+            {{ props.mokup_blog_item_list.nickName }}
+            {{ props.mokup_blog_item_list.writer }}
+          </p>
+        </div>
       </div>
-      <div>
-        <div class="like-value"></div>
+      <div class="like-value">
+        <img :src="icon_black_heart" />
+        {{
+          props.mokup_blog_item_list.like_count.toLocaleString()
+        }}
       </div>
     </div>
   </div>
@@ -25,16 +112,69 @@
 .blog-item-wrapper {
   display: flex;
   flex-direction: column;
+  border-radius: 12px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
   .blog-item-text {
-    min-height: 10rem;
+    height: 9rem;
+    padding: 1.25rem;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    padding: 1rem;
+    .title-wrapper {
+      margin-bottom: 1.5rem;
+      .main-title {
+        font-weight: 700;
+        font-size: 1.25rem;
+      }
+      .sub-title {
+        width: 100%;
+        font-size: 0.9rem;
+        color: #495057;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        line-height: 1.5;
+        -webkit-line-clamp: 4;
+      }
+    }
+    .etc-wrapper {
+      .date-comment {
+        font-size: 0.75rem;
+        color: #868e96;
+      }
+    }
   }
   .blog-item-information {
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    border-top: 1px solid #f1f3f5;
+    .writer-name {
+      span {
+        font-size: 0.75rem;
+        color: #868e96;
+      }
+      p {
+        display: inline;
+        font-size: 0.8rem;
+        font-weight: 700;
+      }
+    }
+    .like-value {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 7px;
+      font-size: 13px;
+    }
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,58 @@
-<script setup></script>
-<template></template>
-<style></style>
+<script setup lang="ts">
+import BlogItemBox from '@components/web/box/BlogItemBox.vue';
+
+const a = [1, 2, 3, 4, 5, 6, 7, 8];
+const mokup_blog_item_list = [
+  {
+    title: 'test 1',
+    sub_title:
+      'sub test 1 sub test 1 sub test 1 sub test 1 sub test 1sub test 1sub test 1 sub test 1sub test 1 sub test 1sub test 1 sub test 1sub test 1sub test 1 sub test 1sub test 1sub test 1 sub test 1 sub test 1 sub test 1sub test 1sub test 1sub test 1sub test 1v sub test 1v sub test 1 sub test 1 vsub test 1 sub test 1 sub test 1 vv sub test 1vsub test 1vvvvv sub test 1sub test 1sub test 1 v vsub test 1 vsub test 1sub test 1sub test 1sub test 1 sub test 1 sub test 1sub test 1 v sub test 1vsub test 1 sub test 1 sub test 1sub test 1v  sub test 1 sub test 1sub test 1 sub test 1sub test 1sub test 1v sub test 1sub test 1 sub test 1  sub test 1sub test 1sub test 1sub test 1sub test 1v v',
+    current_date: '2024-09-24',
+    comment_count: 9,
+    nickName: 'foolish',
+    writer: 'Lee unhak',
+    like_count: 190,
+  },
+  {
+    title: 'test 2',
+    sub_title: 'sub test 2',
+    current_date: '2024-04-24',
+    comment_count: 123,
+    nickName: 'foolish',
+    writer: 'Lee unhak',
+    like_count: 12130,
+  },
+];
+</script>
+<template>
+  <main>
+    <section class="main-list--container">
+      <div v-for="item in mokup_blog_item_list">
+        <BlogItemBox :mokup_blog_item_list="item" />
+      </div>
+    </section>
+  </main>
+</template>
+<style lang="scss" scoped>
+.main-list--container {
+  display: grid;
+  grid-template-columns: repeat(5, minmax(200px, 1fr));
+  gap: 2rem;
+
+  @media (max-width: 1919px) {
+    grid-template-columns: repeat(4, minmax(200px, 1fr));
+  }
+
+  @media (max-width: 1439px) {
+    grid-template-columns: repeat(3, minmax(200px, 1fr));
+  }
+
+  @media (max-width: 1056px) {
+    grid-template-columns: repeat(2, minmax(200px, 1fr));
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, minmax(200px, 1fr));
+  }
+}
+</style>
